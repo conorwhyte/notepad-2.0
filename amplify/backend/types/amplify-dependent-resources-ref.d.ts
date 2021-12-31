7d3958f17aa0df718586d0cff41a57cd11f67aa6
Conor Whyte <conorwt@amazon.com>
2021-12-31 20:43:32 +0000

    Added auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,9 +1,21 @@
 export type AmplifyDependentResourcesAttributes = {
     "api": {
         "notepad20": {
-            "GraphQLAPIKeyOutput": "string",
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "notepad207bc68c33": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
         }
     }
 }
--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import StoreWrapper from './store/StoreWrapper';
 import Header from './components/header/Header';
 import Editor from './components/editor/Editor';
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import './App.css';
 
-function App() {
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+const App = () => {
   return (
-    <StoreWrapper>
-      <Header />
-      <Editor />
-    </StoreWrapper>
+    <Authenticator socialProviders={['google']}>
+      {({ signOut, user }) => (
+        <StoreWrapper>
+          <Header />
+          <Editor />
+        </StoreWrapper>
+      )}
+    </Authenticator>
   );
 }
 
