d593c1466709d85bb357712d12188e7c46b8bf7d
Conor Whyte <conor.whyte@ucdconnect.ie>
2022-10-01 01:30:43 +0100

    Added in service worker

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import reportWebVitals from './reportWebVitals';
 // import { publishWebVitals } from './utils/putMetricData';
 
 render(<App />, document.getElementById('root'));
 
-// For retrigger
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals(publishWebVitals);
+serviceWorkerRegistration.register();
